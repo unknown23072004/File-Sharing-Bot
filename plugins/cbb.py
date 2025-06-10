ba7cc2ee5a5f19431846a861768b0eb710e060a8
unknown23072004 <kiranmanojpandey.kmp@gmail.com>
2025-06-10 21:04:31 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Memes : <a href='https://t.me/MemeTadka>Meme Channel</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Movies : <a href='https://t.me/Cyber_Prime/3'>Click here</a>\n○ Channel 1 : @kylopandey\n○ Channel 2 : @Cyber_Prime</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
